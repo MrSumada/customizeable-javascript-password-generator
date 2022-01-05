3bab3e6111f1c8a32a443ff7b1f109872c5f9616
MrSumada <ricksamericana.com>
2022-01-05 10:41:24 -0800

    fix for loop, now generating password successfully

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,21 @@
-var characters = "abc123!@#";
-var length = 10;
+var characters = "";
+var length = 1;
 
 function generatePassword() {
 
-  var lengthConfirm = window.confirm("Would you like to specify your password's LENGTH?");
+  var lengthConfirm = window.confirm("Would you like to specify your password's LENGTH? Click OK for yes, and CANCEL for no.");
     if (lengthConfirm === true) {
       passwordLength();
+    } else {
+      length = 15;
     };
 
-  var characterConfirm = window.confirm("Would you like to specify your password's character types?");
+  var characterConfirm = window.confirm("Would you like to specify your password's character types? Click OK for yes, and CANCEL for no.");
     if (characterConfirm === true) {
       passwordCharacters();
-    }
+    } else {
+      characters = "abcdefghijklmnopqrtuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*";
+    };
 
   return finalizePassword();
 };
@@ -19,107 +23,76 @@ function generatePassword() {
 //Password length function
 
 function passwordLength() {
-  length = window.prompt("How long should your password be? (Please choose any number from 8 to 128.)");
+  length = window.prompt("How long should your password be? (Please type any number from 8 to 128.)");
   
   length = parseInt(length);
 
-  if (length < 8 || length > 128 ) {
+  if (length < 8 || length > 128) {
     window.alert("Please choose a number from 8 to 128.");
     return passwordLength();
+
+  } else if (!length) {
+    length = 15;
+
   } else {
     window.alert("Gotcha! Your password will be " + length + " characters long!");
     console.log(length);
   };
 };
 
-
 // Password characters function
 
 function passwordCharacters() {
-  var lowerCase = window.confirm("Would you like to include Lowercase characters?");
+  var lowerCase = window.confirm("Would you like to include LOWERCASE characters?");
   console.log(lowerCase);
-  if (lowerCase === true){
-  var upperCase = window.confirm("Would you like to include Uppercase characters?");
+  var upperCase = window.confirm("Would you like to include UPPERCASE characters?");
   console.log(upperCase);
-  };
-  var numericalCharacters = window.confirm("Would you like to include Numerical characters?");
+  var numericalCharacters = window.confirm("Would you like to include NUMERICAL characters like 1, 2, or 3?");
   console.log(numericalCharacters);
-  var specialCharacters = window.confirm("Would you like to include Special characters like !, @, or #?");
+  var specialCharacters = window.confirm("Would you like to include SPECIAL characters like !, @, or #?");
   console.log(specialCharacters);
 
     if (lowerCase === true) {
       characters = "abcdefghijklmnopqrstuvwxyz"
-    } else {
-      characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     }
-    // console.log("1st",characters);
 
     if (upperCase === true) {
       characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + characters  
-    }
-    // console.log("2nd",characters);
+      }
 
     if (numericalCharacters === true) {
       characters = "1234567890" + characters
     }
-    // console.log("3rd",characters);
 
     if (specialCharacters === true) {
       characters = "!@#$%&*" + characters
     }
-    // console.log("4th",characters);
-  // }
-
-  // generatePassword();
+    if (lowerCase === false && upperCase === false && numericalCharacters === false && specialCharacters === false) {
+      window.alert("Please choose at least one character type.");
+      return passwordCharacters();
+    }
 };
-
-// MAKE FROM LENGTH FUNCTION
-//MAKE FROM CHARACTER FUNCTION
-
-
-//Random number generator
-
-// var randomNumber = function(min, max) {
-//   var value = Math.floor(Math.random() * (max - min + 1) + min);
-
-//   return value;
-// };
 
 
 //finalized Password
 
 function finalizePassword() {
   var finalResult = "";
-  const finalLength = length;
-  // var finalCharacters = Array.from(characters);
-  const finalCharacters = characters;
+  var finalLength = length;
+  var finalCharacters = Array.from(characters);
   
   console.log(finalLength);
   console.log(finalCharacters);
-  
-  generateFinalPassword();
 
-  function generateFinalPassword(finalLength) {
-    // let finalResult = '';
-    const characterLength = finalCharacters.length;
-    console.log(characterLength)
+  var random = Math.floor(Math.random() * finalCharacters.length);
 
-    for (let i = 0; i < finalLength; i++ ) {
-      finalResult += finalCharacters.charAt(Math.floor(Math.random() * characterLength));
-    }
-    return finalResult;
+  finalResult = finalCharacters[random];
+
+  for (let i = 0; i < finalLength; i++) {
+    finalResult = finalCharacters[Math.floor(Math.random() * finalCharacters.length)] + finalResult;
   }
-  // var random = Math.floor(Math.random() * finalCharacters.length);
-  // console.log(random);
-  
-  // for (var i = 0; i < finalLength; i++) {
-    
-  // }
-
-  console.log(finalResult);
-
-  return finalResult;
-};
+    return finalResult;
+  };
  
 
 // Get references to the #generate element
